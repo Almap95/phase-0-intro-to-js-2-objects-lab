fc4ff9134d412d45bdcde612f6e37b7c28c24e52
Almap95 <75446860+Almap95@users.noreply.github.com>
2022-04-16 15:52:37 -0400

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+    "name": "Steve",
+    "streetAddress" :"123",
+};
+function updateEmployeeWithKeyAndValue(employeeObj, key, value){
+const newEmployee = { ...employeeObj, [key]: value};
+return newEmployee;
+}
+updateEmployeeWithKeyAndValue (employee, 
+    "name", "NewSteve")
+    
+    const newEmployee = {
+        "name": "Steve",
+        "streetAddress": "123",
+        [key] : value
+    };
+    function destructivelyUpdateEmployeeWithKeyAndValue (employeeObj,key,value){
+    employeeObj [key] = value ;
+        return employeeObj;
+    }
+function deleteFromEmployeeByKey( employeeObj, key){
+    const newEmployee = {...employeeObj};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey (employeeObj, key)
+{ delete employeeObj [key];
+    return employeeObj; 
+}
+deleteFromEmployeeByKey(employee, "name");
+console.log (employee);
